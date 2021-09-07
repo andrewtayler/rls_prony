f27bdad76fa0712da4f3564b5815a02ff5b5a965
andrew <andrewtayler@outlook.com.au>
2021-09-07 14:02:08 +1000

    removed prony sliding window approximation

--- RLS_Prony.py
+++ RLS_Prony.py
@@ -9,6 +9,8 @@ Created on Thu Aug 19 13:46:03 2021
 import numpy as np
 import numpy.polynomial.polynomial as poly
 import sys
+import math
+from scipy.linalg import lstsq
 
 class RLS_Prony:
 
@@ -25,7 +27,7 @@ class RLS_Prony:
         
         # Initialise pseudo inverses
         self.P1 = np.matrix(np.identity(self.M))
-        self.P2 = np.matrix(np.identity(self.M))
+        self.P2 = 10e100*np.matrix(np.identity(self.M))
         
         # Initialise errors
         self.e1 = 0
@@ -35,92 +37,141 @@ class RLS_Prony:
         self.r = np.array(np.zeros([M,1]))
         self.H = np.matrix(np.zeros([M,1]))  
         
-        self.obs_buffer = []#np.matrix(np.zeros([1,M]))
-        self.obs_b2 = []
+        self.obs_buffer = [0]
+        
+        # Fill with buffer with zeros up to the model order
+        while(len(self.obs_buffer) < (self.M)):
+            self.obs_buffer.insert(0,0)  
         
         self.num_obs = 0
         
         self.y_est = 0
         
-    def updateLPM(self,y):
-        lam_inv = 1/self.lam
+    def update(self,y):
+        # Test SSE with current model order
         
-        # Fill with buffer with the first few values
-        if(len(self.obs_buffer) < (self.M)):
-            self.obs_buffer.insert(0,-y)            
-        else:
-            # Get past inputs [y(N-2) y(N-3) ... y(N-M)]
-            un = np.asmatrix(self.obs_buffer).T 
-            
-            Pn_1 = self.P1
-            
-            # Gain
-            K = (lam_inv*Pn_1@un)/(1+lam_inv*un.T@Pn_1@un)
-    
-            # Test to see how the old estimate compares to the new data
-            # y is the new input y(N-1)
-            self.e1 = np.asscalar(y - self.A.T@un)
+        # Increase model order by one if enough error
         
-            # New coefficients are the old + the gain * the error
-            self.A = self.A + K*self.e1 
-        
-            # Update inverse
-            self.P1 = lam_inv*Pn_1 - lam_inv*K@un.T@Pn_1
-            
-            #self.y_est = np.asscalar(self.A.T@un)
-            
-            # Slide the input window along
-            self.obs_buffer.insert(0,-y)
-            self.obs_buffer.pop()
+        # Update
+        self.lpm(y)
+ 
+        # Slide the input window along
+        self.obs_buffer.insert(0,-y)
+        self.obs_buffer.pop()
      
         self.num_obs += 1
         
-    def updateProny(self,i):
+        # if self.num_obs > self.M:         
+        self.prony()
+        
+        
+    def lpm(self,y):
         lam_inv = 1/self.lam
-        y = -self.obs_buffer[0]
         
-        # Find the roots of the characteristic equation
-        c = np.asarray(self.A).reshape(-1)
-        c = np.insert(c,0,1)
-        self.r = poly.polyroots(c)
+        # Get past inputs [y(N-2) y(N-3) ... y(N-M)]
+        un = np.asmatrix(self.obs_buffer).T 
         
+        Pn_1 = self.P1
         
-        # # Remove non-dominant poles
-        # d = r[np.real(r) < 0]
-        # d = np.min(-np.real(d))
-        # Lp = 5*d # Set poles threshold
+        # Gain
+        K = (lam_inv*Pn_1@un)/(1+lam_inv*un.T@Pn_1@un)
+
+        # Test to see how the old estimate compares to the new data
+        # y is the new input y(N-1)
+        self.e1 = np.asscalar(y - self.A.T@un)
+    
+        # New coefficients are the old + the gain * the error
+        self.A = self.A + K*self.e1 
+    
+        # Update inverse
+        self.P1 = lam_inv*Pn_1 - lam_inv*K@un.T@Pn_1
         
-        # self.r = r[np.abs(np.real(r)) <= Lp]
-        # idx = np.where(np.abs(np.real(r)) <= Lp)
+        #self.y_est = np.asscalar(self.A.T@un)
+   
+    def prony(self):
+        lam_inv = 1/self.lam
+        y = -np.array(self.obs_buffer)
         
-        # if len(self.P2) != len(self.r):
-        #     self.P2 = np.matrix(np.identity(len(self.r)))
-        #     self.H = np.matrix(np.zeros([len(self.r),1]))
-        #     print(f'Num roots changed to {len(self.r)} at {i}')
+        self.r = self.get_roots()
             
         # Create new row of the Vandermonde matrix
-        uz = np.asmatrix(np.zeros([len(self.r),1],dtype=np.clongdouble))
-        for j in range(len(self.r)):
-            uz[j,:] = self.r[j]**(self.num_obs-1)
+        uz = np.asmatrix(np.zeros([self.M,len(self.r)],dtype=complex))
         
+        for i in range(self.M):
+            for j in range(len(self.r)):
+                uz[i,j] = self.r[j]**(i)
+            
         # Replace Inf values with max     
         uz = self.remove_inf(uz)
         
-        # Iterate algorithm
-        Pn_z = self.P2
+        #self.H = np.array(np.linalg.lstsq(uz,y)[0])
         
-        K = (lam_inv*Pn_z@uz)/(1+lam_inv*uz.T@Pn_z@uz) # Gain
-        K = self.remove_inf(K)
-    
-        self.e2 = np.asscalar(y - self.H.T@uz) # Test to see how the old estimate compares to the new data
-    
-        self.H = self.H + K*self.e2 # New coefficients are the old + the gain * the error
-    
-        self.P2 = lam_inv*Pn_z - lam_inv*K@uz.T@Pn_z
+        self.H = np.array(lstsq(uz,y)[0])
         
-        self.y_est = np.asscalar(self.H.T@uz)
+        # # Iterate algorithm
+        # #Pn_z = self.P2.copy()
+        # Pn_z = 100*np.matrix(np.identity(self.M))
+        
+        # K = (lam_inv*Pn_z@uz)/(1+lam_inv*uz.T@Pn_z@uz) # Gain
+        # K = self.remove_inf(K)
+    
+        # self.e2 = np.asscalar(y - self.H.T@uz) # Test to see how the old estimate compares to the new data
+    
+        # self.H = K*self.e2
+    
+        # #self.H = self.H + K*self.e2 # New coefficients are the old + the gain * the error
+    
+        # self.P2 = lam_inv*Pn_z - lam_inv*K@uz.T@Pn_z
+        
+        # self.y_est = np.asscalar(self.H.T@uz)
         
         return
+    
+    def get_prony(self):
+        pA = self.H
+        
+        rts = self.get_roots()     
+        Lp = math.inf
+        
+        # Remove non-dominant poles
+        if np.count_nonzero(rts) > 1:
+            # d = rts[np.real(rts) < 0]
+            # d = np.min(-np.real(d))
+            #d = np.min(np.abs(np.real(rts)))
+            d = np.min(rts[np.real(rts) >= 0])
+            Lp = 10*np.real(d) # Set poles threshold
+        
+        #print(Lp)
+        
+        A = []
+        B = []
+        
+        for i in range(len(pA)):           
+            if np.real(np.abs(np.real(rts[i]))) <= Lp:
+
+                B.append(rts[i])
+                A.append(pA[i])
+                
+        B = np.log(np.abs(B))/self.Ts
+        B[np.isneginf(np.real(B))] = 0
+        return A,B
+    
+    def get_est(self,t): 
+        
+        y_est = 0
+        k = (self.num_obs)*self.Ts
+        k = 0
+        
+        A,B = self.get_prony()
+        
+        if len(A) != len(B):
+            print('zero')
+            return 0
+             
+        for j in range(len(A)):
+            y_est = y_est + np.asscalar(A[j]*np.exp(B[j]*k))
+        return y_est
+        
 
     def get_error(self):
         '''
@@ -128,25 +179,19 @@ class RLS_Prony:
         Does not calculate the cumulative effect
         of round-off errors.
         '''
-        return self.e2
+        return self.e1
     
     def get_roots(self):
-        return self.r
-    
-    def get_prony(self):
-        pA = self.H
-        pB = np.log(self.get_roots())/self.Ts
-        return pA,pB
-    
-    def get_est(self,t): 
-        y_est = 0
-        # for A,B in enumerate(zip(*self.get_prony())):
-        #     y_est = y_est + A*np.exp(B*t)
-        A,B = self.get_prony()
-             
-        for j in range(len(A)):
-            y_est = y_est + np.asscalar(A[j]*np.exp(B[j]*t))
-        return y_est
+        # Find the roots of the characteristic equation
+        c = np.asarray(self.A).reshape(-1)
+        c = np.insert(c,0,1)
+        r = poly.polyroots(c)
+        
+        while (self.M > len(r)):
+            r = np.append(r,0)
+        
+        return r
+
     
     def predict(self, x):
         '''
@@ -163,8 +208,11 @@ class RLS_Prony:
         
         return mat_r + 1j*mat_i
     
-    def get_dom_poles(self):
-        r = self.r
+    def get_dom_poles(self,r):
+        
+        if not np.any(r):
+            return r
+        
         # Remove non-dominant poles
         d = r[np.real(r) < 0]
         d = np.min(-np.real(d))
--- test.py
+++ test.py
@@ -8,11 +8,11 @@ Created on Thu Aug 19 14:43:08 2021
 
 import numpy as np
 import matplotlib.pyplot as mpl
-import math
-from RLS_Prony_v2 import RLS_Prony
+from RLS_Prony import RLS_Prony
 import scipy.io as sio
+import math
 
-mat = sio.loadmat('shipData_1.mat')
+mat = sio.loadmat('shipData_3.mat')
 t = np.asarray(mat["t"]).flatten()
 data = np.asarray(mat["Z"]).flatten()
 N = len(t)
@@ -23,8 +23,8 @@ N = len(t)
 # t = np.arange(0,T,T/N)
 # data = np.array([f(i) for i in t])
 
-M = 30 # Number of coefficients including x**0
-lam = 0.6 # Forgetting factor
+M = 10 # Number of coefficients including x**0
+lam = 0.8 # Forgetting factor
 Ts = t[1]-t[0]
 prony = RLS_Prony(M,lam,Ts)
 
@@ -37,39 +37,38 @@ tp = 0
 data_trim = data[0:-tp]
 
 for i,y in enumerate(data):
-    prony.updateLPM(y)
-    if prony.num_obs > prony.M:
-        prony.updateProny(t[i])
-        y_est = prony.get_est(t[i])
-        yt.append(y_est)
-    else:
-        yt.append(0)
+    prony.update(y)
+
     e.append(prony.get_error())
     
-# for i,y in enumerate(t[-tp:]):
-#     yt.append(prony.get_est(y))
+    yt.append(prony.get_est(t[i]))
+    
 
 print(f'Coefficients are: {prony.A}')
 print(f'Roots are: {prony.get_roots()}')
+
+#%% Plotting
 
 fig_2 = mpl.figure()
 er = fig_2.add_axes([0,0,1,1])
 #er.plot(t,np.real(e))
 er.plot(t,e)
 er.set_title('Error')
-er.set_xlim([0,200])
 #er.set_ylim([-10,10])
+#er.set_xlim([0,15])
+
+#%%
 
 fig, yp = mpl.subplots(1, 1, tight_layout=True)
 fig.set_size_inches(8,4)
 yp.plot(t,data,linewidth=2)
 yp.plot(t,np.real(yt),'--',linewidth=3)
-yp.set_xlim([0,200])
 yp.set_ylabel('Heave (m)')
 yp.set_xlabel('Time (s)')
 yp.legend(['Simulated Heave Motion', 'Prony Approximation'])
-fig.savefig('pronyTest.eps')
-#yp.set_ylim([-2,2])
+#fig.savefig('pronyTest.eps')
+#yp.set_ylim([-5,5])
+yp.set_xlim([0,100])
 
 
 #%%
